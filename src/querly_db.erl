3c67c0126fdaee3c285ae5fb81f422572f2f22f7
Dan Mohl <dmohl@yahoo.com>
2010-01-29 11:29:44 -0600

    .

--- src/querly_db.erl
+++ src/querly_db.erl
@@ -1,13 +1,10 @@
 -module(querly_db).
 -author('Dan Mohl').
 
--export([start/0, build_json/2, build_record/3, doc_create/3, doc_create/4, db_info/1, db_create/1, doc_get_all/1, doc_get/2,
-		tables_service/1]).
+-export([start/0, build_json/2, build_record/3, doc_create/3, doc_create/4, doc_get_all/1, doc_get/2,
+		tables_service/1, get_database_names/0, db_exists/1]).
 
 -include_lib("record_definitions.hrl").
-
-get_database_name() ->
-    "test_person_db_load".
 
 start() ->
 	inets:start(),
@@ -22,6 +19,17 @@ build_record(Json, DefaultRecord, RecordFieldNames) ->
 
 tables_service(Table) ->
 	receive
+		{From, get_table, DatabaseName, PrimaryKeyPosition, DefaultRecord, RecordFieldNames} ->
+			case Table of
+				undefined -> 
+					NewTable = ets:new(table, [{keypos, PrimaryKeyPosition}]),
+					build_table_from_couch(DatabaseName, NewTable, DefaultRecord, RecordFieldNames),
+					From ! {table_results, NewTable},
+					tables_service(NewTable);
+				_ -> 
+					From ! {table_results, Table},
+					tables_service(Table)
+			end;
 		{From, get_table, PrimaryKeyPosition, DefaultRecord, RecordFieldNames} ->
 			case Table of
 				undefined -> 
@@ -34,9 +42,13 @@ tables_service(Table) ->
 					tables_service(Table)
 			end
 	end.
-    
+
 build_table_from_couch(Table, DefaultRecord, RecordFieldNames) ->
-	Docs = doc_get_all(get_database_name()),
+	DatabaseName = element(1, DefaultRecord),
+	build_table_from_couch(DatabaseName, Table, DefaultRecord, RecordFieldNames).
+build_table_from_couch(DatabaseName, Table, DefaultRecord, RecordFieldNames) ->
+	db_create_if_needed(DatabaseName),
+	Docs = doc_get_all(DatabaseName),
 	Rows = lists:nth(3, element(2, element(2, Docs))),
 	ResultList = element(2, Rows),	
 	lists:foreach(fun(RowJson) -> 
@@ -45,17 +57,21 @@ build_table_from_couch(Table, DefaultRecord, RecordFieldNames) ->
 		ets:insert(Table, Record) end, ResultList),
 	Table.
 
-db_info(DatabaseName) -> 
-    ecouch:db_info(DatabaseName).
+db_create_if_needed(DatabaseName) ->
+    case db_exists(DatabaseName) of
+		false -> ecouch:db_create(DatabaseName);
+		_ -> ok
+	end.	
 
-db_create(DatabaseName) ->
-    ecouch:db_create(DatabaseName).
+db_exists(DatabaseName) ->
+    lists:any(fun(DbName) -> bitstring_to_list(DbName) == DatabaseName end, get_database_names()).
 
+doc_create(DocName, RecordToTransform, RecordFieldNames) ->
+	DatabaseName = atom_to_list(element(1, RecordToTransform)),
+	doc_create(DatabaseName, DocName, RecordToTransform, RecordFieldNames).
 doc_create(DatabaseName, DocName, RecordToTransform, RecordFieldNames) ->
 	Json = querly_db:build_json(RecordToTransform, RecordFieldNames),
-    doc_create(DatabaseName, DocName, Json).	
-doc_create(DatabaseName, DocName, Json) ->
-    ecouch:doc_create(DatabaseName, DocName, Json).
+    ecouch:doc_create(DatabaseName, DocName, Json).	
     
 doc_get_all(DatabaseName) ->    
 	ecouch:doc_get_all(DatabaseName, [{include_docs,true}]).
@@ -63,4 +79,5 @@ doc_get_all(DatabaseName) ->
 doc_get(DatabaseName, DocName) ->    
 	ecouch:doc_get(DatabaseName, DocName).	
 	
-
+get_database_names() ->
+	element(2, ecouch:db_list()).
--- tests/querly_db_tests.erl
+++ tests/querly_db_tests.erl
@@ -2,8 +2,7 @@
 -author('Dan Mohl').
 
 -export([run_all/0, test_build_record/0, test_build_json/0, test_create_person_doc/0,
-		test_create_person_doc_with_json/0, test_db_info/0, test_simple_doc_create/0,
-		test_get_all_docs/0, test_should_load_table_records/0]).
+		test_get_all_docs/0, test_should_load_table_records/0, test_get_database_names/0, test_db_should_exist/0]).
 		 
 -include_lib("../src/record_definitions.hrl").
 
@@ -16,7 +15,7 @@ delete_test_db() ->
 initialize_test_suite() ->
     querly_db:start(),	
 	delete_test_db(),
-	querly_db:db_create(get_test_db_name()).
+	ecouch:db_create(get_test_db_name()).
 
 finalize_test_suite() ->
     delete_test_db().
@@ -27,12 +26,11 @@ run_all() ->
 	% all tests
 	test_build_json(),
 	test_build_record(),
-	test_db_info(),
-	test_simple_doc_create(),
-	test_create_person_doc_with_json(),
 	test_create_person_doc(),
 	test_get_all_docs(),
 	test_should_return_expected_person_record(),
+	test_get_database_names(),
+	test_db_should_exist(),
 	test_should_load_table_records(),
 	% cleanup
 	finalize_test_suite(),
@@ -53,19 +51,6 @@ test_build_record() ->
 	Result = querly_db:build_record(Json, DefaultRecord, RecordFieldNames),
 	test_helper:display_message({"test_build_record", Result == PersonRecord, Result}).
 	
-test_db_info() ->
-    Result = querly_db:db_info(get_test_db_name()),	
-	test_helper:display_message({"test_db_info", element(1, Result) == ok, Result}). 
-
-test_simple_doc_create() ->	
-	Result = querly_db:doc_create(get_test_db_name(), "A", {obj, [{"foo", 1}]}),
-	test_helper:display_message({"test_simple_doc_create", element(1, Result) == ok, element(2, Result)}).
-	
-test_create_person_doc_with_json() ->	
-	Json = {obj, [{"idno", 99999996}, {"firstName", "Dan"}, {"lastName", "Mohl"}, {"dob", "08/28/1977"}, {"ssn", "123-45-9876"}]},
-	Result = querly_db:doc_create(get_test_db_name(), "1", Json),
-	test_helper:display_message({"test_create_person_doc_with_json", element(1, Result) == ok, element(2, Result)}).
-
 test_create_person_doc() ->	
 	PersonRecord = #person{idno=99999997, firstName="Dan", lastName="Mohl", dob="08/28/1977", ssn="123-45-9876"},
 	RecordFieldNames = record_info(fields, person),
@@ -80,28 +65,52 @@ test_get_all_docs() ->
 	test_helper:display_message({"test_get_all_docs", element(1, Result) == ok, element(2, Result)}).
 
 test_should_return_expected_person_record() ->	
-	Json = {obj, [{"idno", 99999999}, {"firstName", "Dan"}, {"lastName", "Mohl"}, {"dob", "08/28/1977"}, {"ssn", "123-45-9876"}]},
 	PersonRecord = #person{idno=99999999, firstName="Dan", lastName="Mohl", dob="08/28/1977", ssn="123-45-9876"},
 	RecordFieldNames = record_info(fields, person),
-	querly_db:doc_create(get_test_db_name(), "4", Json),
+	querly_db:doc_create(get_test_db_name(), "3", PersonRecord, RecordFieldNames),
 	ResultJson = element(2, querly_db:doc_get(get_test_db_name(), "4")),
 	Result = querly_db:build_record(ResultJson, PersonRecord, RecordFieldNames),
 	test_helper:display_message({"test_should_return_expected_person_record", Result == PersonRecord, Result}).
 	
+test_get_database_names() ->
+	ecouch:db_create("test_get_database_names_1"),
+	ecouch:db_create("test_get_database_names_2"),
+	ecouch:db_create("test_get_database_names_3"),
+	DatabaseNameList = querly_db:get_database_names(),
+	Result = erlang:length(DatabaseNameList),
+	ecouch:db_delete("test_get_database_names_1"),
+	ecouch:db_delete("test_get_database_names_2"),
+	ecouch:db_delete("test_get_database_names_3"),
+	test_helper:display_message({"test_get_database_names", Result >= 3, Result}).
+
+test_db_should_exist() ->
+	ecouch:db_create("person_exist"),
+    Result = querly_db:db_exists("person_exist"),
+	ecouch:db_delete("person_exist"),
+	test_helper:display_message({"test_db_should_exist", Result == true, Result}).
+
 test_should_load_table_records() ->
-	io:format("Started test_should_load_table_records...Please wait...~n"),
+	PersonRecord1 = #person{idno=99999995, firstName="Dan", lastName="Mohl", dob="08/28/1977", ssn="123-45-9876"},
+	PersonRecord2 = #person{idno=99999996, firstName="Dan2", lastName="Mohl", dob="08/28/1977", ssn="123-45-9876"},
+	PersonRecord3 = #person{idno=99999997, firstName="Dan3", lastName="Mohl", dob="08/28/1977", ssn="123-45-9876"},
+	RecordFieldNames = record_info(fields, person),
+	querly_db:doc_create("99999995", PersonRecord1, RecordFieldNames),
+	querly_db:doc_create("99999996", PersonRecord2, RecordFieldNames),
+	querly_db:doc_create("99999997", PersonRecord3, RecordFieldNames),
 	Pid = spawn(querly_db, tables_service, [undefined]),
 	DefaultRecord = #person{},
 	RecordFieldNames = record_info(fields, person),
 	Pid ! {self(), get_table, #person.idno, DefaultRecord, RecordFieldNames},
 	receive
-		{table_results, PeopleTable} ->
-			People = PeopleTable;
+		{table_results, Table} ->
+			People = Table;
 		_ -> 
 			People = ets:new(people, [{keypos, #person.idno}])
 	end,
 	ResultSet = ets:select(People, [{#person{_ = '_'}, [], ['$_']}]),
 	Result = erlang:length(ResultSet),
-	test_helper:display_message({"test_should_load_table_records", Result > 0, Result}).
+	ecouch:db_delete("person"),
+	test_helper:display_message({"test_should_load_table_records", Result == 3, Result}).
 	
 	
+    	
