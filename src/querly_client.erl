fae1276bf48b5248c31dfd79e8fbedcfaf2ad1fe
Dan Mohl <dmohl@yahoo.com>
2010-02-09 11:41:45 -0600

    Fixed issues with the load test and added functionality to auto start querly when a query is run.

--- src/querly_client.erl
+++ src/querly_client.erl
@@ -50,14 +50,14 @@ select(RecordName, WhereElements) ->
 			Criteria = parse_where_clause(RecordName, WhereElements, RecordFieldNames),
 			DefaultRecord = transform_to_record([], list_to_atom(RecordName), '_', RecordFieldNames),
 			PrimaryKeyIndex = 2,
-			%io:format("Criteria: ~p~n", [Criteria]),
-			%io:format("DefaultRecord: ~p~n", [DefaultRecord]),
-			%io:format("RecordFieldNames: ~p~n", [RecordFieldNames]),
-			%io:format("PrimaryKeyIndex: ~p~n", [PrimaryKeyIndex]),
+			start_querly(),
 			querly:select(Criteria, DefaultRecord, RecordFieldNames, PrimaryKeyIndex);
 		false ->	
 			[]
 	end.
+
+start_querly() ->
+	querly:start().
 	
 parse_sql(Sql) ->
 	SelectList = parse_sql_select(Sql),
--- tests/querly_load_tests.erl
+++ tests/querly_load_tests.erl
@@ -1,7 +1,7 @@
 -module(querly_load_tests).
 -author('Dan Mohl').
 
--export([run_all/0, load_10000_people/0]).
+-export([run_all/0, load_10000_people/0, load_10000_employers/0]).
 		 
 -include_lib("../src/record_definitions.hrl").
 
@@ -9,28 +9,46 @@ get_test_db_name_format() ->
 	"test_~s_db_load".
 
 initialize_test_suite() ->
-    querly:start(get_test_db_name_format()).
+    %querly:start(get_test_db_name_format()).
+	querly:start().
 
 run_all() ->
 	% initialize tests
 	initialize_test_suite(),
 	% all tests
-	load_10000_people().
+	load_10000_people(),
+	load_10000_employers().
 
 load_10000_people() ->	
-	Iterations = 10000, 
+	Iterations = 9999, 
 	create_person(1, Iterations).
 
+load_10000_employers() ->	
+	Iterations = 9999, 
+	create_employer(1, Iterations).
+
 create_person(Index, Iterations) ->
-	FirstName = io_lib:format("~p~p", ["TestFirst", Index]),
-	LastName = io_lib:format("~p~p", ["TestLast", Index]),
+	FirstName = binary_to_list(list_to_binary(io_lib:format("TestFirst~p", [Index]))),
+	LastName = binary_to_list(list_to_binary(io_lib:format("TestLast~p", [Index]))),
 	PersonRecord = #person{idno=Index, firstName=FirstName, lastName=LastName, dob="08/28/1977", ssn="123-45-9876"},			
 	RecordFieldNames = record_info(fields, person),
-	querly:doc_create(io_lib:format("~p", [Index]), PersonRecord, RecordFieldNames),
+	querly_db:doc_create(io_lib:format("~p", [Index]), PersonRecord, RecordFieldNames),
 	case (Index =< Iterations) of
 		true -> 
 			create_person(Index+1, Iterations),
-			io:format("Created ~p of ~p~n", [Index, Iterations]);
+			io:format("Created ~p of ~p People~n", [(Iterations - Index) + 2, Iterations + 1]);
 		_ -> done
 	end.
 
+create_employer(Index, Iterations) ->
+	EmployerName = binary_to_list(list_to_binary(io_lib:format("TestEmployer~p", [Index]))), 
+	Address = binary_to_list(list_to_binary(io_lib:format("~p Main Street", [Index]))), 
+	EmployerRecord = #employer{id=Index, name=EmployerName, address=Address},			
+	RecordFieldNames = record_info(fields, employer),
+	querly_db:doc_create(io_lib:format("~p", [Index]), EmployerRecord, RecordFieldNames),
+	case (Index =< Iterations) of
+		true -> 
+			create_employer(Index+1, Iterations),
+			io:format("Created ~p of ~p Employers~n", [(Iterations - Index) + 2, Iterations + 1]);
+		_ -> done
+	end.
